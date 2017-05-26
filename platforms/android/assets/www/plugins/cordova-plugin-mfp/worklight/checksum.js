f8585ae1b67ffc05b394ddf39e5b2b27c246f2ed
Angel Angeles <aangeles@aginc.local>
2017-05-26 16:18:56 -0400

    second commit

---
+++ platforms/android/assets/www/plugins/cordova-plugin-mfp/worklight/checksum.js
@@ -0,0 +1,1 @@
+var WL_CHECKSUM = {"checksum":0,"date":1495829184727,"machine":"agincdsk21"}
---
+++ platforms/android/assets/www/plugins/cordova-plugin-mfp/worklight/static_app_props.js
@@ -0,0 +1,20 @@
+// This is a generated file. Do not edit. See application-descriptor.xml.
+// WLClient configuration variables.
+console.log("Running static_app_props.js...");
+var WL = WL ? WL : {};
+WL.StaticAppProps = { APP_ID: 'tone.analyzer.app',
+  APP_VERSION: '0.0.1',
+  WORKLIGHT_PLATFORM_VERSION: '8.0.0.00-20170328-091204',
+  WORKLIGHT_NATIVE_VERSION: '2957450250',
+  LANGUAGE_PREFERENCES: 'en',
+  ENVIRONMENT: 'preview',
+  PREVIEW_ENVIRONMENT: 'android',
+  PREVIEW_ROOT_URL: 'http://172.16.200.161:9080/mfp/',
+  APP_SERVICES_URL: '/mfp/apps/services/',
+  POSTFIX_APP_SERVICES_URL: '/mfp/apps/services/',
+  WORKLIGHT_ROOT_URL: '/mfp/apps/services/api/Tone Analyzer App/android/',
+  POSTFIX_WORKLIGHT_ROOT_URL: '/mfp/apps/services/api/Tone Analyzer App/android/',
+  APP_DISPLAY_NAME: 'Tone Analyzer App',
+  LOGIN_DISPLAY_TYPE: 'embedded',
+  mfpClientCustomInit: false,
+  MESSAGES_DIR: 'plugins\\cordova-plugin-mfp\\worklight\\messages' };
