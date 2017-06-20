4e92fb0d11f0de8df90db21903492efd8d10814f
commit3 <danie@aangeles>
2017-06-19 22:35:30 -0400

    tercer commit

--- www/js/validation.js
+++ www/js/validation.js
@@ -41,28 +41,29 @@ $(function(){
 
 $(document).ready(function(){
 	$( "#registrationForm" ).submit(function( event ) {
-		
-		var name=$("#name").val();
-		var email=$("#email").val();
-		var password=$("#password").val();
-		
-		var dataString="name="+name+"&email="+email+"&password="+password+"&insert=";
-		
-				$.ajax({
-	   	   		 type: "POST",
-	   	   		 url: "http://localhost/tone_analyzer/database/user_management/register.php",
-	   	   		 data: dataString,
-	   	   		 crossDomain: true,
-	   	   		 cache: false,
-	   	   		 beforeSend: function(){ $("#submit-button").val('Connecting...');},
-	   	   		 success: function(data){
-	   	   			 if(data=="success"){
-	   	   				 	window.location.replace("../UC002-login/login.html");
-	   	   				 }
-	   	   				 else if(data=="error"){
-	   	   					 alert("error");
-	   	   				 }
-	   	   			 }
-	   	   		 });
+		if($(this).valid()) {
+			var name=$("#name").val();
+			var email=$("#email").val();
+			var password=$("#password").val();
+			
+			var dataString="name="+name+"&email="+email+"&password="+password+"&insert=";
+			
+					$.ajax({
+		   	   		 type: "POST",
+		   	   		 url: "http://localhost/tone_analyzer/database/user_management/register.php",
+		   	   		 data: dataString,
+		   	   		 crossDomain: true,
+		   	   		 cache: false,
+		   	   		 beforeSend: function(){ $("#submit-button").val('Connecting...');},
+		   	   		 success: function(data){
+		   	   			 if(data=="success"){
+		   	   				 	window.location.replace("../UC002-login/login.html");
+		   	   				 }
+		   	   				 else if(data=="error"){
+		   	   					 alert("error");
+		   	   				 }
+		   	   			 }
+		   	   		 });
+		}
 	});
 });
